26933572b7e6345b7f62e7462e1ca839dd46425a
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-09-12 00:40:32 +0200

    Stateful

--- src/HigherLower.hs
+++ src/HigherLower.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
+
 module HigherLower where
+
+import Control.Monad.State
 
 data Status = Won | Lost | Active { turns :: Int } deriving (Show, Eq)
 
@@ -8,23 +12,20 @@ data Game = Game
             , status :: Status
             } deriving (Show)
 
-guess :: Game -> Int -> (Maybe Ordering, Game)
-guess g@(Game (l, u) number status) i
-  | status == Lost = (Nothing, g)
-  | status == Lost = (Nothing, g)
-  | otherwise =
-    let
-      result = compare i number
-      nextStatus = newStatus status result
-      nextBounds = case result of
-        EQ -> (l, u)
-        LT -> (i, u)
-        GT -> (l, i)
-    in (Just result, Game nextBounds number nextStatus)
-
-isActive :: Status -> Bool
-isActive (Active _) = True
-isActive _ = False
+guess :: Int -> State Game (Maybe Ordering)
+guess i = state $
+          \case g@(Game (l, u) secret status)
+                  | status == Lost -> (Nothing, g)
+                  | status == Won -> (Nothing, g)
+                  | otherwise ->
+                    let
+                      result = compare i secret
+                      nextBounds = case result of
+                        EQ -> (l, u)
+                        LT -> (i, u)
+                        GT -> (l, i)
+                      nextStatus = newStatus status result
+                    in (Just result, Game nextBounds secret nextStatus)
 
 newStatus :: Status -> Ordering -> Status
 newStatus (Active i) EQ
