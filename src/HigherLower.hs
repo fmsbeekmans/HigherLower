9bf990e76ca203f70341cc27086a75599dff4f3c
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-09-11 23:27:41 +0200

    track bounds

--- src/HigherLower.hs
+++ src/HigherLower.hs
@@ -3,16 +3,24 @@ module HigherLower where
 data Status = Won | Lost | Active { turns :: Int } deriving (Show, Eq)
 
 data Game = Game
-            { secret :: Int
+            { bounds :: (Int, Int)
+            , secret :: Int
             , status :: Status
             } deriving (Show)
 
 guess :: Game -> Int -> (Maybe Ordering, Game)
-guess g@(Game number status) i
+guess g@(Game (l, u) number status) i
   | status == Lost = (Nothing, g)
   | status == Lost = (Nothing, g)
-  | otherwise = let result = compare i number
-                in (Just result, Game number $ newStatus status result)
+  | otherwise =
+    let
+      result = compare i number
+      nextStatus = newStatus status result
+      nextBounds = case result of
+        EQ -> (l, u)
+        LT -> (i, u)
+        GT -> (l, i)
+    in (Just result, Game nextBounds number nextStatus)
 
 isActive :: Status -> Bool
 isActive (Active _) = True
