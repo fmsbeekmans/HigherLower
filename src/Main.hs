503152e39da146fb3d2d4a76cf7e350b55102011
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-09-13 23:59:37 +0200

    Main wip

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,14 @@ module Main where
 
 import Text.Read
 import System.Random
+import Control.Monad.State
 
 import HigherLower.Game
 
 main :: IO ()
 main = do
   g <- createGame
+--  runState (mfix play) $ asStateTIO g
   return ()
   
 createGame :: IO Game
@@ -18,8 +20,16 @@ createGame = do
   secret <- getStdRandom (randomR (low, high))
   return (Game (low, high) turns (Active turns))
 
-play :: Int -> StateT Game IO ()
-    
+play :: StateT Game IO () -> StateT Game IO ()
+play state = do
+  i <- asStateTIO $ retryPrompt "Not a number" (promptInt "How many tries do you get?")
+  result <- guess i
+  
+  return ()
+
+asStateTIO :: IO a -> StateT Game IO a
+asStateTIO = liftIO
+
 promptInt :: String ->  IO (Maybe Int)
 promptInt q = do
   putStrLn q
