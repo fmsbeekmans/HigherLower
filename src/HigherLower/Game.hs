5c80589eb3b722ec877be4d31c360768e20a2c9d
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-09-28 22:14:00 +0200

    checkGuess written with do

--- src/HigherLower/Game.hs
+++ src/HigherLower/Game.hs
@@ -11,19 +11,42 @@ data Game = Game
             } deriving (Show)
 
 checkGuess :: (Monad m) => Int -> StateT Game m (Maybe Ordering)
-checkGuess i = state step
-  where
-    step g@(Game (l, u) secret status)
-      | status == Lost = (Nothing, g)
-      | status == Won = (Nothing, g)
-      | otherwise = (Just result, Game nextBounds secret nextStatus)
-      where
+checkGuess i = do
+  g <- get
+  case g of
+    (Game b_ _ Lost) -> return Nothing
+    (Game _ _ Won) -> return Nothing
+    (Game bounds secret status) ->
+      let
         result = compare i secret
-        nextBounds = case result of
-          EQ -> (l, u)
-          LT -> (i, u)
-          GT -> (l, i)
+        nextBounds = newBounds bounds secret result
         nextStatus = newStatus status result
+      in do
+        put $ Game nextBounds secret nextStatus
+        return $ Just result
+
+        
+    
+      
+--checkGuess i = state step
+--  where
+--    step g@(Game (l, u) secret status)
+--      | status == Lost = (Nothing, g)
+--      | status == Won = (Nothing, g)
+--      | otherwise = (Just result, Game nextBounds secret nextStatus)
+--      where
+--        result = compare i secret
+--        nextBounds = case result of
+--          EQ -> (l, u)
+--          LT -> (i, u)
+--          GT -> (l, i)
+--        nextStatus = newStatus status result
+
+newBounds :: (Int, Int) -> Int -> Ordering -> (Int, Int)
+newBounds (l, u) i o
+  | o == EQ = (i, i)
+  | o == LT = (i, u)
+  | o == GT = (l, i)
 
 newStatus :: Status -> Ordering -> Status
 newStatus (Active i) EQ
